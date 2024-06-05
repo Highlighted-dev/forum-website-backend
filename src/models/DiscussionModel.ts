275fd44c48a26f0b50e3e723bdf9d2215b358657
Highlighted-dev <highlighted.priv@gmail.com>
2024-06-05 19:40:18 +0200

    Added "category" to discussion model

--- src/models/DiscussionModel.ts
+++ src/models/DiscussionModel.ts
@@ -4,17 +4,22 @@ export interface IDiscussionModel extends Document {
   username: string;
   title: string;
   content: string;
-  create_date: Date;
-  edit_date: Date;
+  createdAt: Date;
+  editedAt: Date;
+  pinned: boolean;
+  category: string;
   answers: IDiscussionModel[];
 }
 
 const discussionsSchema: Schema = new Schema(
   {
+    title: { type: String, required: true },
     username: { type: String, required: true },
     content: { type: String, required: true },
-    create_date: { type: Date, default: Date.now },
-    edit_date: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: Date.now },
+    pinned: { type: Boolean, default: false },
+    category: { type: String, required: true },
     answers: [
       {
         type: {
