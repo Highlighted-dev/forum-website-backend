d66b50f1e0764cc6d4f5548438068450da76d779
Highlighted-dev <highlighted.priv@gmail.com>
2024-06-06 15:11:10 +0200

    Added a cron job that should stop the server from sleeping

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import { errorHandlingMiddleware } from "middlewares/errorHandlingMiddleware";
 import { morganMiddleware } from "middlewares/morganMiddleware";
 import ChatRoute from "routes/ChatRoute";
 import DiscussionRoute from "routes/DiscussionRoute";
-
+import { getCurrentUrl } from "utils/getCurrentUrl";
+import cron from "node-cron";
+import { logger } from "utils/logger";
 export const app: Application = express();
 
 app.use(morganMiddleware);
@@ -13,3 +15,14 @@ app.use(apiKeyMiddleware);
 app.use("/externalApi/chat/", ChatRoute);
 app.use("/externalApi/discussion/", DiscussionRoute);
 app.use("/", (req, res) => res.send("Hello World!"));
+
+// free servers hosted on render go to sleep after 15 minutes of inactivity, which means the first request sent to the server after these 15 minutes may take up to one minute.
+// We dont want that
+const stopServerFromSleep = async () => {
+  logger.info("Stopping server from sleeping...");
+  await fetch(getCurrentUrl() + "/", {
+    method: "GET",
+    headers: { "x-api-key": process.env.API_KEY_TOKEN },
+  });
+};
+cron.schedule("*/14 * * * *", stopServerFromSleep);
---
+++ src/utils/getCurrentUrl.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const getCurrentUrl = (): string => {
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:5000";
+  }
+  return process.env.API_URL_PROD;
+};
